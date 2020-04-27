643e33c98de9619ad54c4806ff0695a21256381d
BenHall-7 <benjaminjahall@gmail.com>
2020-04-26 21:55:59 -0400

    fix my cookies not being sent

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ server.use(
         secret: 'nobody tosses a dwarf!',
         cookie: {
             maxAge: 1000*60*5, // 5 minutes
-            secure: true, // only set cookies over https. Server will not send back a cookie over http.
+            secure: false, // only set cookies over https. Server will not send back a cookie over http.
+            httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
         },
-        httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
         resave: false,
         saveUninitialized: false,
     })
